6fe767b60b8bff8863f808c5d46c0fdb7f9dd95e
Raymond Lo <raymond.lo@yahoo.com>
2023-01-24 00:03:58 -0500

    Complete product CRUD routes and controllers

--- src/Services/Catalog/src/controllers/product.ts
+++ src/Services/Catalog/src/controllers/product.ts
@@ -3,7 +3,66 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const getProduct = async (req: Request, res: Response) => {};
+export const getProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const productId = Number(id);
+
+    const product = await prisma.product.findUnique({
+      where: {
+        id: productId,
+      },
+    });
+
+    if (product === null) {
+      res.status(404).json({
+        message: "Product Not Found",
+      });
+    }
+
+    res.status(200).json({
+      data: product,
+    });
+  } catch (error) {
+    res.status(400).json({ message: "Bad Request" });
+  }
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const productId = Number(id);
+
+    await prisma.product.delete({
+      where: {
+        id: productId,
+      },
+    });
+
+    res.setHeader("Location", `/product/${id}`);
+    res.status(204).send();
+  } catch (error) {
+    res.status(400).json({ message: "Bad Request" });
+  }
+};
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { name, price, imageUrl } = req.body;
+    const productId = Number(id);
+
+    const updatedProduct = await prisma.product.update({
+      where: { id: productId },
+      data: { name, price, imageUrl },
+    });
+
+    res.status(200).json({
+      data: updatedProduct,
+    });
+  } catch (error) {
+    res.status(400).json({ message: "Bad Request" });
+  }
+};
 
 export const createProduct = async (req: Request, res: Response) => {
   try {
@@ -15,12 +74,11 @@ export const createProduct = async (req: Request, res: Response) => {
         imageUrl,
       },
     });
-    return res.json({
-      success: true,
+    res.status(200).json({
       data: product,
     });
   } catch (error) {
-    return res.json({
+    res.status(400).json({
       success: false,
       message: error,
     });
--- src/Services/Catalog/src/routes/product.ts
+++ src/Services/Catalog/src/routes/product.ts
@@ -1,9 +1,16 @@
 import express from "express";
-import { createProduct, getProduct } from "../controllers/product";
+import {
+  createProduct,
+  deleteProduct,
+  getProduct,
+  updateProduct,
+} from "../controllers/product";
 
 const router = express.Router();
 
-router.get("/", getProduct);
+router.get("/:id", getProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 router.post("/", createProduct);
 
 export default router;
