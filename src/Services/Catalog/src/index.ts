7f3643bf498f7e279b8ac37194cd51f53f5d9299
Raymond Lo <raymond.lo@yahoo.com>
2023-01-26 18:15:20 -0500

    Clean up entry file

--- src/Services/Catalog/src/index.ts
+++ src/Services/Catalog/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import productRouter from "./routes/product";
 import collectionRouter from "./routes/collection";
 
