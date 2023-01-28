b333ed13a5165fca0aca2b5c3f89a4c5a4497ec0
Raymond Lo <raymond.lo@yahoo.com>
2023-01-28 11:23:49 -0500

    Add a layout to app

---
+++ src/Web/layouts/layout.tsx
@@ -0,0 +1,18 @@
+import Header from "../components/header/Header";
+import { ReactNode } from "react";
+// import Footer from "./footer";
+
+type LayoutProps = {
+  children?: ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
