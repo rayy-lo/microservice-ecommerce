dad61e0bead86a76319035fccf3cea21685fc3d3
Raymond Lo <raymond.lo@yahoo.com>
2023-01-30 00:23:30 -0500

    Add Google font for application

--- src/Web/pages/_app.tsx
+++ src/Web/pages/_app.tsx
@@ -1,11 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Cormorant_Garamond } from "@next/font/google";
 import Layout from "../layouts/layout";
+
+const inter = Cormorant_Garamond({ weight: ["400", "700"], style: ["normal"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
