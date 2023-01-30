d4df9d707a16c5a7e5099cd7edb56bc213363e66
Raymond Lo <raymond.lo@yahoo.com>
2023-01-30 02:55:21 -0500

    Header component

---
+++ src/Web/components/header/Header.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../public/logo-no-background.png";
+import Cart from "../../public/shopping-bag.png";
+import User from "../../public/user.png";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  const { nav, header, leftNav, rightNav, logoLink } = styles;
+
+  return (
+    <header className={header}>
+      <nav className={nav}>
+        <div className={leftNav}>
+          <Link className={logoLink} href="/">
+            <Image width={120} src={Logo} alt="Comme Logo" />
+          </Link>
+          <Link href="/products/cat-castle">Cat Castle</Link>
+          <Link href="/products/feline-fortress">Feline Fortress</Link>
+          <Link href="/collections/cat-beds">Cat Beds</Link>
+        </div>
+        <div className={rightNav}>
+          <Link href="/login">
+            <Image width={25} src={User} alt="Login" />
+          </Link>
+          <Link href="/cart">
+            <Image width={26} height={26} src={Cart} alt="Cart" />
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
