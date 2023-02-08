56160a644dae0d2c399f16721e5a2b4631d0e6a9
Raymond Lo <raymond.lo@yahoo.com>
2023-02-08 17:30:51 -0500

    Return on data for responses

--- src/Services/Catalog/src/helpers/response.ts
+++ src/Services/Catalog/src/helpers/response.ts
@@ -2,9 +2,9 @@ import { Response } from "express";
 import { DataObject } from "../types/types";
 
 export const sendSuccess = (res: Response, data: DataObject) => {
-  res.status(200).json({ data });
+  res.status(200).json(data.data);
 };
 
 export const sendError = (res: Response, data: DataObject) => {
-  res.status(400).json({ message: data.message });
+  res.status(400).json(data.message);
 };
