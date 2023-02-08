93b317bed3ac6b192244d02b8057deecf630d01e
Raymond Lo <raymond.lo@yahoo.com>
2023-02-08 17:34:10 -0500

    Add page for products

---
+++ src/Web/pages/product/[handle].tsx
@@ -0,0 +1,50 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/ProductPage.module.css";
+
+export default function ProductPage({ product }) {
+  const {
+    productContainer,
+    productDetails,
+    productName,
+    price,
+    imageContainer,
+    description,
+  } = styles;
+
+  return (
+    <>
+      <Head>
+        <title>{product.name}</title>
+      </Head>
+      <section className={productContainer}>
+        <div className={imageContainer}>
+          <Image priority fill src={product.imageUrl} alt={product.title} />
+        </div>
+        <div className={productDetails}>
+          <h2 className={productName}>{product.name}</h2>
+          <span className={price}>${product.price}</span>
+          <p className={description}>{product.description}</p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export async function getServerSideProps({ req, res, params }) {
+  const response = await fetch(
+    `${process.env.CATALOG_API_URL}/api/product/${params.handle}`
+  );
+  const product = await response.json();
+
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=59"
+  );
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
