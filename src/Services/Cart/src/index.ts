495b94d560760ccc8daebe645501fad0d37e0b65
Raymond Lo <raymond.lo@yahoo.com>
2023-02-13 18:29:44 -0500

    Start Cart API with redis sessions

--- src/Services/Cart/src/index.ts
+++ src/Services/Cart/src/index.ts
@@ -1,7 +1,33 @@
 import express, { Application } from "express";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { createClient } from "redis";
 
 const app: Application = express();
+app.use(express.json());
 
-app.listen(3002, () => {
+const RedisStore = connectRedis(session);
+const client = createClient({
+  url: process.env.REDIS_DB_URL,
+});
+
+client.on("error", function (err) {
+  console.log("Could not establish a connection with redis. " + err);
+});
+client.on("connect", function (err) {
+  console.log("Connected to redis successfully");
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "secret",
+    resave: true,
+    saveUninitialized: false,
+    store: new RedisStore({ client }),
+  })
+);
+
+app.listen(3002, async () => {
   console.log("Listening on port 3002");
+  await client.connect();
 });
