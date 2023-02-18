5c009d21cebf921109816cc3c382192dcb92ab26
Raymond Lo <raymond.lo@yahoo.com>
2023-02-17 23:27:41 -0500

    Remove sessions implementation
    It's not required in our application.

--- src/Services/Cart/src/index.ts
+++ src/Services/Cart/src/index.ts
@@ -1,36 +1,23 @@
 import express, { Application } from "express";
-import session from "express-session";
-import connectRedis from "connect-redis";
 import { createClient } from "redis";
 import cartRouter from "./routes/cart";
 
 const app: Application = express();
 app.use(express.json());
-
 app.use("/api/cart", cartRouter);
 
-const RedisStore = connectRedis(session);
-const client = createClient({
+export const redisClient = createClient({
   url: process.env.REDIS_DB_URL,
 });
 
-client.on("error", function (err) {
+redisClient.on("error", function (err) {
   console.log("Could not establish a connection with redis. " + err);
 });
-client.on("connect", function (err) {
+redisClient.on("connect", function (err) {
   console.log("Connected to redis successfully");
 });
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET || "secret",
-    resave: true,
-    saveUninitialized: false,
-    store: new RedisStore({ client }),
-  })
-);
-
 app.listen(3002, async () => {
   console.log("Listening on port 3002");
-  await client.connect();
+  await redisClient.connect();
 });
