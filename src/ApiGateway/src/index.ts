97d08c3ba32efec3d64c1292f37894e73d2dbcec
Raymond Lo <raymond.lo@yahoo.com>
2023-03-02 20:23:19 -0500

    Add proxy for cart microservice

--- src/ApiGateway/src/index.ts
+++ src/ApiGateway/src/index.ts
@@ -1,10 +1,16 @@
 import express, { Application } from "express";
-import cartRouter from "./routes/cart";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app: Application = express();
 const PORT = 3004;
 
-app.use("/cart", cartRouter);
+app.use(
+  "/cart",
+  createProxyMiddleware({
+    target: `${process.env.CART_API_URL}/api`,
+    changeOrigin: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log("API Gateway running on 3004");
