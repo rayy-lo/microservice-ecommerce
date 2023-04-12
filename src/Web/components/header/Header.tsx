acc74cd5427e6697af0bae26b2b407c7c96ef436
Raymond Lo <raymond.lo@yahoo.com>
2023-04-11 21:51:09 -0400

    feat(header): mobile menu component

--- src/Web/components/header/Header.tsx
+++ src/Web/components/header/Header.tsx
@@ -3,32 +3,93 @@ import Image from "next/image";
 import Logo from "../../public/logo-no-background.png";
 import Cart from "../../public/icons/shopping-bag.png";
 import User from "../../public/icons/user.png";
+import Hamburger from "../../public/icons/hamburger.svg";
 import styles from "./Header.module.css";
+import { useState, MouseEventHandler, useRef } from "react";
+import { CSSTransition } from "react-transition-group";
 
 const Header = ({ toggleCart }) => {
-  const { nav, header, leftNav, rightNav, logoLink } = styles;
+  const {
+    nav,
+    header,
+    leftNav,
+    rightNav,
+    logoLink,
+    button,
+    mobileMenu,
+    mobileMenuBtn,
+  } = styles;
+
+  const nodeRef = useRef(null);
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const toggleMobileMenu: MouseEventHandler = (event) => {
+    setMobileMenuOpen((prevState) => !prevState);
+  };
 
   return (
-    <header className={header}>
-      <nav className={nav}>
-        <div className={leftNav}>
-          <Link className={logoLink} href="/">
-            <Image width={120} src={Logo} alt="Comme Logo" />
-          </Link>
-          <Link href="/collection/best-sellers">Best Sellers</Link>
-          <Link href="/collection/featured">Featured</Link>
-          <Link href="/collection/sale">Sale</Link>
+    <>
+      <header className={header}>
+        <nav className={nav}>
+          <ul className={leftNav}>
+            <Link className={logoLink} href="/">
+              <Image width={120} src={Logo} alt="Comme Logo" />
+            </Link>
+            <Link href="/collection/best-sellers">Best Sellers</Link>
+            <Link href="/collection/featured">Featured</Link>
+            <Link href="/collection/sale">Sale</Link>
+          </ul>
+          <ul className={rightNav}>
+            <Link href="/account/login">
+              <Image width={25} src={User} alt="Login" />
+            </Link>
+            <button className={button} onClick={toggleCart} type="button">
+              Cart
+              <Image width={25} src={Cart} alt="Cart" />
+            </button>
+            <button
+              onClick={toggleMobileMenu}
+              className={`${button} ${mobileMenuBtn}`}
+              type="button"
+            >
+              Menu
+              <Image width={25} src={Hamburger} alt="Menu" />
+            </button>
+          </ul>
+        </nav>
+      </header>
+      <CSSTransition
+        appear
+        nodeRef={nodeRef}
+        unmountOnExit
+        in={isMobileMenuOpen}
+        timeout={{
+          enter: 200,
+          exit: 200,
+        }}
+        classNames={{
+          ...styles,
+        }}
+      >
+        <div className={mobileMenu} ref={nodeRef}>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/account/login">My Account</Link>
+              </li>
+              <li>
+                <Link href="/collection/best-sellers">Best Sellers</Link>
+              </li>
+              <li>
+                <Link href="/collection/featured">Featured</Link>
+              </li>
+              <li>
+                <Link href="/collection/sale">Sale</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <div className={rightNav}>
-          <Link href="/account/login">
-            <Image width={25} src={User} alt="Login" />
-          </Link>
-          <button onClick={toggleCart} type="button">
-            <Image width={25} src={Cart} alt="Cart" />
-          </button>
-        </div>
-      </nav>
-    </header>
+      </CSSTransition>
+    </>
   );
 };
 
